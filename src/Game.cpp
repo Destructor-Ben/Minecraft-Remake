e3ea6d9b105b29d2887fd37af5fd9651595d57e6
Destructor_Ben <ben.honeyfield@gmail.com>
2024-02-22 20:27:46 +1300

    Update Game.cpp

--- src/Game.cpp
+++ src/Game.cpp
@@ -67,7 +67,7 @@ namespace Minecraft
     {
         UpdateInput();
 
-        if (IsKeyDown(Key::Space))
+        if (WasKeyReleased(Key::Space))
             glfwSetWindowShouldClose(window, true);
     }
 
