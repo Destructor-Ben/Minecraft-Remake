00465e3d5fe1946ce1499a3d4af1fbee8f1f4d80
olmarsh <marsh.oliver@hotmail.com>
2024-03-05 10:54:20 +1300

    add block in chunk functions

--- src/World/Chunk.cpp
+++ src/World/Chunk.cpp
@@ -1,2 +1,12 @@
 #include "Chunk.h"
 
+namespace Minecraft
+{
+    Block Chunk::GetBlockInChunk(uint8_t BlockX, uint8_t BlockY, uint8_t BlockZ) {
+        return blocks_[BlockX*ChunkSize*ChunkSize + BlockY*ChunkSize + BlockZ];
+    };
+
+    void Chunk::SetBlockInChunk(uint8_t BlockX, uint8_t BlockY, uint8_t BlockZ, Block Block) {
+        blocks_[BlockX*ChunkSize*ChunkSize + BlockY*ChunkSize + BlockZ] = Block;
+    };
+}
--- src/World/Chunk.h
+++ src/World/Chunk.h
@@ -3,17 +3,16 @@
 #include "../Common.h"
 #include "Block.h"
 
-uint8_t const ChunkX = 16;
-uint8_t const ChunkY = 16;
-uint8_t const ChunkZ = 16;
+uint8_t const ChunkSize = 16;
 
 namespace Minecraft
 {
     class Chunk
     {
         private:
-            Block blocks_[ChunkX*ChunkY*ChunkZ];
+            Block blocks_[ChunkSize*ChunkSize*ChunkSize];
         public:
-            uint16_t GetChunk();
+            Block GetBlockInChunk(uint8_t BlockX, uint8_t BlockY, uint8_t BlockZ);
+            void SetBlockInChunk(uint8_t BlockX, uint8_t BlockY, uint8_t BlockZ, Block Block);
     };
 }
