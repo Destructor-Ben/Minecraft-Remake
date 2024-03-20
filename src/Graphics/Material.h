d211ae1d8f9539d4dc1ea2725ff39ba73fe996a0
Destructor_Ben <ben.honeyfield@gmail.com>
2024-03-21 09:28:15 +1300

    Fix CMakeLists.txt

--- src/Graphics/Material.h
+++ src/Graphics/Material.h
@@ -8,7 +8,7 @@ namespace Minecraft
 {
     // TODO: make this virtual so we can use inheritance to allow more material types
     // Materials exist so you can set a bunch of fields, then bind will automatically bind it to the shader
-    // It means you don't need to bind the shader, set the uniforms, then pass the shader into a draw function, since the uniforms are bundled with it
+    // It means you don't need to bind the shader to set the uniforms, since the uniforms are bundled with it
     class Material
     {
     public:
