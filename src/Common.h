4ea6ae0dd1e8c7419dc2eb21f4fc92ea4f3fc284
Destructor_Ben <ben.honeyfield@gmail.com>
2024-03-21 09:22:50 +1300

    Update common.h

--- src/Common.h
+++ src/Common.h
@@ -16,6 +16,9 @@
 #include <iostream>
 #include <string>
 #include <cassert>
+#include <cmath>
+#include <vector>
+#include <list>
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -32,14 +35,15 @@ namespace Minecraft
     using glm::vec3;
     using glm::vec4;
 
-    typedef signed char        int8;
-    typedef signed short       int16;
-    typedef signed int         int32;
-    typedef signed long long   int64;
-    typedef unsigned char      uint8;
-    typedef unsigned short     uint16;
-    typedef unsigned int       uint32;
-    typedef unsigned long long uint64;
+    typedef int8_t int8;
+    typedef int16_t int16;
+    typedef int32_t int32;
+    typedef int64_t int64;
+
+    typedef uint8_t uint8;
+    typedef uint16_t uint16;
+    typedef uint32_t uint32;
+    typedef uint64_t uint64;
 
     inline void Log(const string& message)
     {
