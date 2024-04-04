332b2e4166376f1a3f8e89a16362e4dc26ec3d71
Destructor_Ben <ben.honeyfield@gmail.com>
2024-04-05 11:53:38 +1300

    Make VertexBuffer SetData const

--- src/Graphics/VertexBuffer.h
+++ src/Graphics/VertexBuffer.h
@@ -10,7 +10,7 @@ namespace Minecraft
         VertexBuffer();
         ~VertexBuffer();
 
-        void SetData(const void* data, uint32 size, GLenum usage = GL_STATIC_DRAW) const;
+        void SetData(const void* data, uint32 size, GLenum usage = GL_STATIC_DRAW);
 
         void Bind() const;
 
