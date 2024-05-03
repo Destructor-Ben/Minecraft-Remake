124ee7dbfdeae049df0b70b6dbe4e47372a49223
Destructor_Ben <ben.honeyfield@gmail.com>
2024-05-03 18:38:31 +1200

    Update Noise.cpp

--- src/Noise/Noise.cpp
+++ src/Noise/Noise.cpp
@@ -1,18 +1,23 @@
 #include "Noise.h"
 
-namespace Minecraft {
+#include <numbers>
+
+namespace Minecraft
+{
     // Hugo Elias hash function
-    double IntNoise2D(uint32 seed, int32 x, int32 y) {
+    double IntNoise2D(uint32 seed, int32 x, int32 y)
+    {
         int n = x + y * 57 + seed;
-        n = ( n << 13 ) ^ n;
-        double ret = 1 - ( (n * (n * n * 15731 + 789221) + 1376312589) & 0x7fffffff) / 1073741824.0f;
+        n = (n << 13) ^ n;
+        double ret = 1 - ((n * (n * n * 15731 + 789221) + 1376312589) & 0x7fffffff) / 1073741824.0f;
         return ret;
     }
 
     // Cosine interpolation function. Returns point x between a and b
-    double CosineInterpolate(double a, double b, double x) {
-        double ft = x * M_PI;
+    double CosineInterpolate(double a, double b, double x)
+    {
+        double ft = x * std::numbers::pi;
         double f = (1 - cos(ft)) * .5;
-        return  a*(1-f) + b*f;
+        return a * (1 - f) + b * f;
     }
 }
