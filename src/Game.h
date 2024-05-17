1e56bda3f76c4708bbb0effbae2961aaecc4a51c
Destructor_Ben <ben.honeyfield@gmail.com>
2024-05-17 12:23:08 +1200

    Update Game.h

--- src/Game.h
+++ src/Game.h
@@ -9,12 +9,15 @@
 
 namespace Minecraft
 {
+    class LogManager;
     class InputManager;
+    class Renderer;
+    class World;
 
     inline shared_ptr<LogManager> Logger = nullptr;
     inline shared_ptr<InputManager> Input = nullptr;
-    inline shared_ptr<class Renderer> Renderer = nullptr;
-    inline shared_ptr<class World> World = nullptr;
+    inline shared_ptr<Renderer> Renderer = nullptr;
+    inline shared_ptr<World> World = nullptr;
 
     inline bool Running = true;
     inline std::thread::id MainThreadID = std::thread::id();
