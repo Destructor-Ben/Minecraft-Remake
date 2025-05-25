25a1d054aab27244c439a1beae9c28c7ce800fea
Destructor_Ben <ben.honeyfield@gmail.com>
2025-05-25 16:07:50 +1200

    Finish main menu

--- src/UI/States/UIMainMenu.cpp
+++ src/UI/States/UIMainMenu.cpp
@@ -15,25 +15,21 @@ namespace Minecraft
     {
         IsInGameUI = false;
 
-        m_ExitButton = make_shared<UIButton>();
-        m_ExitButton->SetText("Test!");
-        m_ExitButton->x.Percent = 0.5f;
-        m_ExitButton->y.Percent = 0.5f;
-        m_ExitButton->OriginX.Percent = 0.5f;
-        m_ExitButton->OriginY.Percent = 0.5f;
-        AddElement(m_ExitButton);
-
-        /* TODO: reneable old UI
+        // Repeating dirt texture
         auto backgroundTexture = Instance->Resources->RequestTexture("ui/menu/background");
+        backgroundTexture->SetWrappingModes(GL_REPEAT);
         m_Background = make_shared<UISprite>();
         m_Background->DrawnSprite.SpriteTexture = backgroundTexture;
+        m_Background->DrawnSprite.UVs = Rectangle(vec2i(0), Instance->ScreenSize / 30);
+        m_Background->Width.Percent = 1;
+        m_Background->Height.Percent = 1;
         AddElement(m_Background);
 
         auto logoTexture = Instance->Resources->RequestTexture("ui/menu/logo");
         m_Logo = make_shared<UISprite>();
-        m_Logo->SetTexture(logoTexture);
+        m_Logo->SetTexture(logoTexture, vec2(1.25));
         m_Logo->x.Percent = 0.5f;
-        m_Logo->y.Pixels = -300;
+        m_Logo->y.Pixels = -200;
         m_Logo->y.Percent = 1.0f;
         m_Logo->OriginX.Percent = 0.5f;
         m_Logo->OriginY.Percent = 0.5f;
@@ -55,18 +51,22 @@ namespace Minecraft
         m_PlayButton->y.Percent = 1.0f;
         m_PlayButton->OriginX.Percent = 0.5f;
         m_PlayButton->OriginY.Percent = 0.5f;
+        m_PlayButton->Width.HasMin = true;
+        m_PlayButton->Width.MinPixels = 300;
         m_PlayButton->SetText("Play");
         AddElement(m_PlayButton);
 
         m_ExitButton = make_shared<UIButton>();
         m_ExitButton->OnMouseUp = []() { Instance->Close(); };
         m_ExitButton->x.Percent = 0.5f;
-        m_ExitButton->y.Pixels = -700;
+        m_ExitButton->y.Pixels = -500 - m_PlayButton->GetSize().y - 10;
         m_ExitButton->y.Percent = 1.0f;
         m_ExitButton->OriginX.Percent = 0.5f;
         m_ExitButton->OriginY.Percent = 0.5f;
+        m_ExitButton->Width.HasMin = true;
+        m_ExitButton->Width.MinPixels = 300;
         m_ExitButton->SetText("Exit");
-        AddElement(m_ExitButton);*/
+        AddElement(m_ExitButton);
     }
 
     void UIMainMenu::CheckActive()
